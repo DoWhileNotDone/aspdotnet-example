fa3e4f16fa08301ac77ee7a4f658e46f9a807de4
DavegTheMighty <dave@example.com>
2023-04-07 16:40:22 +0100

    First Commit

---
+++ Pages/HelloWorld.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+public class HelloWorldModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public HelloWorldModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
